00658acce56fd228d96a2f49e3b4828767443291
firebird11 <hbgassel@gmail.com>
2016-07-30 12:42:54 +0200

    change back to cm

--- crdroid.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/htc/m8/full_m8.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
-
-# Inherit some common crdroid stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
-
-PRODUCT_NAME := crdroid_m8
